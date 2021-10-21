702bb01aa30ecbb7af8b5acb71239150b647c30a
Nazeer Ahmad <69080659+nazo21@users.noreply.github.com>
2021-10-21 14:54:44 -0400

    Add files via upload

---
+++ Lab2.cpp
@@ -0,0 +1,11 @@
+//displays a greeting
+// YOUR NAME
+// TODAY's DATE
+
+#include <iostream>
+
+using std::cout; using std::endl;
+
+int main() {
+	cout << "Hello, I am Nazeer Ahmad! How are you?" << endl;
+}
